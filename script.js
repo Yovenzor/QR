f56c84f6e9ce7d06a64f3e653698e15a6a939ba2
Yovenzor Singh <102180213+Yovenzor@users.noreply.github.com>
2023-12-26 13:38:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue = "";
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if (!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+        const downloadButton = document.querySelector(".download-button");
+        downloadButton.style.display = "block";
+        const downloadLink = document.getElementById("downloadLink");
+        downloadLink.href = qrImg.src;
+});
+});
+
+qrInput.addEventListener("input", () => {
+    let qrValue = qrInput.value.trim();
+    if (!qrValue) {
+        wrapper.classList.remove("active");
+        preValue = ""; 
+        return;
+}
+
+qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+});
+
+qrInput.addEventListener("keyup", () => {
+if (!qrInput.value.trim()) {
+    wrapper.classList.remove("active");
+    preValue = "";
+}
+});
